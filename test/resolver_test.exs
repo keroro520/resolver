3e33141159f97fb402143957207f60862cb43245
keroro520 <guozhen.wang@upai.com>
2017-08-08 12:14:07 +0800

    update tests

--- test/resolver_test.exs
+++ test/resolver_test.exs
@@ -1,8 +1,13 @@
 defmodule ResolverTest do
   use ExUnit.Case
 
+  @resolve_size 100
+
   test "resolve www.upyun.com" do
     assert {:ok, _addr} = Resolver.resolve("www.upyun.com")
+    for index <- 0..@resolve_size-1 do
+      assert [{{^index, "www.upyun.com"}, _}] = :ets.lookup(:resolve_ets, {index, "www.upyun.com"})
+    end
   end
 
   test "resolve www.invalid_upyun.com" do
@@ -10,13 +15,19 @@ defmodule ResolverTest do
   end
 
   test "when nxdomain, reuse the previous addresses" do
-    hostaddrs = for index <- 0..2000, do: {{index, "www.reuse_upyun.com"}, "127.0.0.1"}
+    hostaddrs = for index <- 0..@resolve_size-1, do: {{index, "www.reuse_upyun.com"}, "127.0.0.1"}
     :ets.insert(:resolve_ets, hostaddrs)
 
     assert {:ok, _addr} = Resolver.resolve("www.reuse_upyun.com")
+    for index <- 0..@resolve_size-1 do
+      assert [{{^index, "www.reuse_upyun.com"}, "127.0.0.1"}] = :ets.lookup(:resolve_ets, {index, "www.reuse_upyun.com"})
+    end
     assert {:error, :nxdomain} = GenServer.call(Resolver, {:resolve, "www.reuse_upyun.com"})
     send(Resolver, :resolve)
     :timer.sleep(1000)
     assert {:ok, _addr} = Resolver.resolve("www.reuse_upyun.com")
+    for index <- 0..@resolve_size-1 do
+      assert [{{^index, "www.reuse_upyun.com"}, "127.0.0.1"}] = :ets.lookup(:resolve_ets, {index, "www.reuse_upyun.com"})
+    end
   end
 end
